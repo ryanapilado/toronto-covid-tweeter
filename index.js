ff24d8c944c1b312cc8658b26346a767851f35bf
Ryan Apilado <ryan.apilado@gmail.com>
2020-11-15 19:14:22 -0500

    get 7day avg working

--- index.js
+++ index.js
@@ -3,42 +3,58 @@ const axios = require('axios');
 const dayjs = require('dayjs');
 const utc = require('dayjs/plugin/utc');
 const timezone = require('dayjs/plugin/timezone');
+dayjs.extend(utc);
+dayjs.extend(timezone);
 
-exports.tweetReport = (req, res) => {
+exports.tweetReport = async (req, res) => {
 
   let date;
   if (req.query.hasOwnProperty('date')) {
-    date = req.query.date;
+    date = dayjs(req.query.date).tz("America/Toronto");
   } else {
-    date = dayjs().tz("America/Toronto").format("YYYY-MM-DD");
+    date = dayjs().tz("America/Toronto");
   }
 
-  const reportURL = process.env.READ_REPORT_ENDPOINT + '?date=' + date;
-
-  axios.get(reportURL)
-    .then(function (response) {
-
-      const twitter = new twit({
-        consumer_key: process.env.CONSUMER_KEY,
-        consumer_secret: process.env.CONSUMER_SECRET,
-        access_token: process.env.ACCESS_TOKEN,
-        access_token_secret: process.env.ACCESS_TOKEN_SECRET
-      });
-
-      let message = `${response.data.torontoNewCases} new cases of COVID-19 in Toronto yesterday, and ${response.data.ontarioNewCases} in Ontario.  #toronto #covid19 #coronavirus`;
-      twitter.post('statuses/update', { status: message }, function(error, data, response) {
-
-        if (error) {
-          res.status(500).send(error);
-        }
-
-        res.status(200).send();
-
-      });
+  const responses = await Promise.all(
+    Array.from(Array(7).keys()).map(i => {
+        const dateString = date.subtract(i, 'day').format("YYYY-MM-DD");
+        const reportURL = process.env.READ_REPORT_ENDPOINT + '?date=' + dateString;
+        return axios.get(reportURL)
     })
+  )
 
-    .catch(function (error) {
-      console.error(error);
+  const torontoNewCases = responses[0].data.torontoNewCases;
+  const ontarioNewCases = responses[0].data.ontarioNewCases;
+  const toronto7DayAvg = get7DayAvg(responses, 'torontoNewCases');
+  const ontario7DayAvg = get7DayAvg(responses, 'ontarioNewCases');
+
+  const twitter = new twit({
+    consumer_key: process.env.CONSUMER_KEY,
+    consumer_secret: process.env.CONSUMER_SECRET,
+    access_token: process.env.ACCESS_TOKEN,
+    access_token_secret: process.env.ACCESS_TOKEN_SECRET
+  });
+
+  let message = 
+    `${torontoNewCases} new cases of COVID-19 in Toronto yesterday` +
+    ` and ${ontarioNewCases} in Ontario.` +
+    ` 7-day averages are ${toronto7DayAvg} and ${ontario7DayAvg} respectively.` +
+    ` #toronto #covid19 #coronavirus`;
+
+  twitter.post('statuses/update', { status: message }, function(error, data, response) {
+
+    if (error) {
       res.status(500).send(error);
-    });
+    }
+
+    res.status(200).send();
+
+  });
+
+}
+
+function get7DayAvg(responses, key) {
+  const sum = responses.map(response => response.data[key])
+    .reduce((a, b) => a + b, 0);
+  return (sum / responses.length).toFixed(2);
 }
